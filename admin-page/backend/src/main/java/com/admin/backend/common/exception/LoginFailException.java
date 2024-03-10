cf02ff82b7cf681a1073acc5104fd5700566e90d
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-11 00:01:17 +0900

    doc : Login Fail Exception 주석

--- admin-page/backend/src/main/java/com/admin/backend/common/exception/LoginFailException.java
+++ admin-page/backend/src/main/java/com/admin/backend/common/exception/LoginFailException.java
@@ -1,5 +1,8 @@
 package com.admin.backend.common.exception;
 
+/**
+ * Login Fail Exception
+ */
 public class LoginFailException extends RuntimeException{
     public LoginFailException(String message) {
         super(message);
