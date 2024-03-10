3e13a0a32b9b3695407c8d748cfd367b4df4ae47
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-11 00:02:32 +0900

    doc : document 주석 추가

--- admin-page/backend/src/main/java/com/admin/backend/common/exception/handler/GlobalControllerExceptionHandler.java
+++ admin-page/backend/src/main/java/com/admin/backend/common/exception/handler/GlobalControllerExceptionHandler.java
@@ -4,9 +4,18 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+/**
+ * Global Controller Exception Handler
+ */
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
+    /**
+     * LoginFailException Handler
+     * @param loginFailException
+     * @param redirectAttributes
+     * @return
+     */
     @ExceptionHandler(LoginFailException.class)
     public String handleLoginFailException(LoginFailException loginFailException, RedirectAttributes redirectAttributes){
         redirectAttributes.addFlashAttribute("errorMessage",loginFailException.getMessage());
