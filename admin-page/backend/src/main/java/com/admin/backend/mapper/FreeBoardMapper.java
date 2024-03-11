0c9bfad0f84c79adf81fe12493a26bd071d8a14b
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-12 03:08:26 +0900

    feat : selectTotalRowCountByCondition, selectBoardListByCondition 추가

--- admin-page/backend/src/main/java/com/admin/backend/mapper/FreeBoardMapper.java
+++ admin-page/backend/src/main/java/com/admin/backend/mapper/FreeBoardMapper.java
@@ -1,10 +1,31 @@
 package com.admin.backend.mapper;
 
+import com.admin.backend.dto.FreeBoardDto;
+import com.admin.backend.dto.SearchConditionDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * tb_free_board DB mapper
  */
 @Mapper
 public interface FreeBoardMapper {
+
+    /**
+     * 검색조건에 맞는 자유게시물의 총 개수 SELECT
+     *
+     * @param searchConditionDto
+     * @return
+     */
+    int selectTotalRowCountByCondition(SearchConditionDto searchConditionDto);
+
+    /**
+     * 검색조건과 페이지네이션에 맞는 자유게시물 리스트 SELECT
+     *
+     * @param searchConditionDto
+     * @return
+     */
+    List<FreeBoardDto> selectBoardListByCondition(SearchConditionDto searchConditionDto);
+
 }
