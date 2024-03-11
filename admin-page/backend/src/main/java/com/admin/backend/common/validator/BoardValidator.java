955ed96f1d6871776f501f6a5438c7efec7b9ea4
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-11 14:01:09 +0900

    feat : Board input data 유효성 검증 class

---
+++ admin-page/backend/src/main/java/com/admin/backend/common/validator/BoardValidator.java
@@ -0,0 +1,34 @@
+package com.admin.backend.common.validator;
+
+import com.admin.backend.common.exception.IllegalBoardDataException;
+import com.admin.backend.dto.NoticeBoardDto;
+
+/**
+ * Board input data validator
+ */
+public class BoardValidator {
+
+    private static final int NOTICE_TITLE_MIN = 1;
+    private static final int NOTICE_TITLE_MAX = 99;
+    private static final int NOTICE_CONTENT_MIN = 1;
+    private static final int NOTICE_CONTENT_MAX = 3999;
+
+    /**
+     * Notice Board Validator
+     *
+     * @param noticeBoardDto
+     */
+    public static void validateNoticeBoard(NoticeBoardDto noticeBoardDto) {
+        validateText(noticeBoardDto.getTitle(), NOTICE_TITLE_MIN, NOTICE_TITLE_MAX);
+        validateText(noticeBoardDto.getContent(), NOTICE_CONTENT_MIN, NOTICE_CONTENT_MAX);
+    }
+
+    private static void validateText(String text, int minLength, int maxLength) {
+        if (text == null) {
+            throw new IllegalBoardDataException("입력칸이 비어있습니다.");
+        } else if (text.length() < minLength || text.length() > maxLength) {
+            // TODO : errorMsg
+            throw new IllegalBoardDataException("데이터 입력 오류.");
+        }
+    }
+}
