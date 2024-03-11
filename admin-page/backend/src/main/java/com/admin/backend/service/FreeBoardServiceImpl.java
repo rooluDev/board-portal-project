ac5593176f664c9661e682b5bd55c8b1d4a465f4
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-11 19:04:57 +0900

    init : FreeBoardService 구현체

---
+++ admin-page/backend/src/main/java/com/admin/backend/service/FreeBoardServiceImpl.java
@@ -0,0 +1,10 @@
+package com.admin.backend.service;
+
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Free Board Service 구현체
+ */
+@Primary
+public class FreeBoardServiceImpl {
+}
