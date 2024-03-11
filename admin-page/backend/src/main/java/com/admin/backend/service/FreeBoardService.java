32f4475456050b9a319102cc06823c4ada77e83f
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-12 03:08:11 +0900

    feat : getTotalRowCountByCondition, getBoardListByCondition 추가

--- admin-page/backend/src/main/java/com/admin/backend/service/FreeBoardService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/FreeBoardService.java
@@ -1,10 +1,28 @@
 package com.admin.backend.service;
 
-import org.springframework.stereotype.Service;
+import com.admin.backend.dto.FreeBoardDto;
+import com.admin.backend.dto.SearchConditionDto;
+
+import java.util.List;
 
 /**
  * Free Board Service Interface
  */
-@Service
 public interface FreeBoardService {
+
+    /**
+     * 검색조건에 맞는 자유게시물의 총 개수 가져오기
+     *
+     * @param searchConditionDto
+     * @return
+     */
+    int getTotalRowCountByCondition(SearchConditionDto searchConditionDto);
+
+    /**
+     * 검색조건과 페이지네이션에 맞는 자유게시물 리스트 가져오기
+     *
+     * @param searchConditionDto
+     * @return
+     */
+    List<FreeBoardDto> getBoardListByCondition(SearchConditionDto searchConditionDto);
 }
