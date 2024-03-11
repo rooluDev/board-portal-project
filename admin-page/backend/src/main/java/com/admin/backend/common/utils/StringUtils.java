5077e01422a7db8225e9a8f4462875b7be0a8f93
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-11 18:35:32 +0900

    feat : 사용자 정의 String Util Class

---
+++ admin-page/backend/src/main/java/com/admin/backend/common/utils/StringUtils.java
@@ -0,0 +1,28 @@
+package com.admin.backend.common.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Custom String Utils
+ */
+public class StringUtils {
+
+    /**
+     * uri에 있는 숫자 덩어리를 추출하는 메소드
+     *
+     * @param uri
+     * @return
+     */
+    public static List<String> extractNumberFromUri(String uri){
+        List<String> numberList = new ArrayList<>();
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(uri);
+        while (matcher.find()){
+            numberList.add(matcher.group());
+        }
+        return numberList;
+    }
+}
