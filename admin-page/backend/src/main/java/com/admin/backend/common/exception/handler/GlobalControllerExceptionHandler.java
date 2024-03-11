f0c0a72cb4e28ac9c31ff059e8e069f86bdae2a8
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-11 14:06:41 +0900

    feat : IllegalBoardDataException, FixedBoardFullException handler 추가

--- admin-page/backend/src/main/java/com/admin/backend/common/exception/handler/GlobalControllerExceptionHandler.java
+++ admin-page/backend/src/main/java/com/admin/backend/common/exception/handler/GlobalControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.admin.backend.common.exception.handler;
 
+import com.admin.backend.common.exception.FixedBoardFullException;
+import com.admin.backend.common.exception.IllegalBoardDataException;
 import com.admin.backend.common.exception.LoginFailException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,13 +15,46 @@ public class GlobalControllerExceptionHandler {
 
     /**
      * LoginFailException Handler
+     *
      * @param loginFailException
      * @param redirectAttributes
      * @return
      */
     @ExceptionHandler(LoginFailException.class)
-    public String handleLoginFailException(LoginFailException loginFailException, RedirectAttributes redirectAttributes){
-        redirectAttributes.addFlashAttribute("errorMessage",loginFailException.getMessage());
+    public String handleLoginFailException(LoginFailException loginFailException, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("errorMessage", loginFailException.getMessage());
+
         return "redirect:/admin/login";
+    }
+
+    /**
+     * FixedBoardFullException Handler
+     *
+     * @param fixedBoardFullException
+     * @param redirectAttributes
+     * @return
+     */
+    @ExceptionHandler(FixedBoardFullException.class)
+    public String handleFixedBoardFullException(FixedBoardFullException fixedBoardFullException, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("errorMessage", fixedBoardFullException.getMessage());
+        // TODO : 검색조건 잃어버림
+        return "redirect:/admin/board/notice/write";
+    }
+
+    /**
+     * IllegalBoardDataException Handler
+     *
+     * @param illegalBoardDataException
+     * @param redirectAttributes
+     * @return
+     */
+    @ExceptionHandler(IllegalBoardDataException.class)
+    public String handleIllegalBoardDataException(IllegalBoardDataException illegalBoardDataException, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("errorMessage", illegalBoardDataException.getMessage());
+
+        return "redirect:/admin/board/notice/write";
     }
 }
