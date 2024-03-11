18c93c746e6900bf03a7c9e325f287b68b889623
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-12 03:08:41 +0900

    feat : Free Board Dto 멤버변수 추가

--- admin-page/backend/src/main/java/com/admin/backend/dto/FreeBoardDto.java
+++ admin-page/backend/src/main/java/com/admin/backend/dto/FreeBoardDto.java
@@ -1,7 +1,27 @@
 package com.admin.backend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
 /**
  * Free Board Dto
  */
+@Getter
+@Setter
 public class FreeBoardDto {
+    private Long boardId;
+    private Long categoryId;
+    private String authorType;
+    private String authorId;
+    private String title;
+    private String content;
+    private int views;
+    private Timestamp createdAt;
+    private Timestamp editedAt;
+    private String categoryName; // list page에 필요한 카테고리 이름
+    private Long fileId; // list page에 필요한 파일 여부를 위한 파일 아이디
+    private String adminName; // list page에 필요한 등록자 이름
+    private String memberName; // list page에 필요한 등록자 이름
 }
