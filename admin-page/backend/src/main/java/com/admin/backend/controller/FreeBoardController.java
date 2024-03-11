42f0274db974ae3f6e9b3fa59dc1b34d15bc696e
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-11 18:47:13 +0900

    init : Free Board Controller

---
+++ admin-page/backend/src/main/java/com/admin/backend/controller/FreeBoardController.java
@@ -0,0 +1,15 @@
+package com.admin.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Free Board Controller
+ */
+@Controller
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class FreeBoardController {
+
+}
