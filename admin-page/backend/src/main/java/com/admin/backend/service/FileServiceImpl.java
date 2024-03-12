d6ccca89c7d5b6a4169b4da6378de0030dbbc5b0
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-12 23:27:36 +0900

    feat : addFile 구현

--- admin-page/backend/src/main/java/com/admin/backend/service/FileServiceImpl.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/FileServiceImpl.java
@@ -1,10 +1,47 @@
 package com.admin.backend.service;
 
+import com.admin.backend.common.utils.MultipartFileUtils;
+import com.admin.backend.dto.FileDto;
+import com.admin.backend.mapper.FileMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * File Service 구현체
  */
 @Primary
-public class FileServiceImpl {
+@Service
+@RequiredArgsConstructor
+public class FileServiceImpl implements FileService{
+
+    public static final String PATH = "/Users/user/upload/";
+    private final FileMapper fileMapper;
+
+    @Override
+    public List<FileDto> addFile(MultipartFile[] fileList, String boardType, Long boardId) {
+        List<FileDto> addedFileList = new ArrayList<>();
+        for(MultipartFile file : fileList){
+            if(!file.isEmpty()){
+                FileDto fileDto = FileDto.builder()
+                        .boardType(boardType)
+                        .boardId(boardId)
+                        .originalName(file.getOriginalFilename())
+                        .physicalName(UUID.randomUUID().toString())
+                        .filePath(PATH)
+                        .extension(MultipartFileUtils.extractExtension(file))
+                        .size(file.getSize())
+                        .build();
+                fileMapper.insertFile(fileDto);
+                addedFileList.add(fileDto);
+            }
+        }
+        return addedFileList;
+    }
 }
