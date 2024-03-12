9afa97f55519c44efe44743519697436b7d61d80
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-12 23:27:20 +0900

    feat : addFile 추가

--- admin-page/backend/src/main/java/com/admin/backend/service/FileService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/FileService.java
@@ -1,10 +1,22 @@
 package com.admin.backend.service;
 
-import org.springframework.stereotype.Service;
+import com.admin.backend.dto.FileDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * File Service Interface
  */
-@Service
 public interface FileService {
+
+    /**
+     * boardType에 boardId에 파일 추가
+     *
+     * @param fileList
+     * @param boardType
+     * @param boardId
+     * @return
+     */
+    List<FileDto> addFile(MultipartFile[] fileList, String boardType, Long boardId);
 }
