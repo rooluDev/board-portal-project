db327b6d1184302e0c6a428335669f071cc92dd5
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-13 21:58:40 +0900

    feat : CommentDto 멤버변수 추가

--- admin-page/backend/src/main/java/com/admin/backend/dto/CommentDto.java
+++ admin-page/backend/src/main/java/com/admin/backend/dto/CommentDto.java
@@ -1,7 +1,24 @@
 package com.admin.backend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
 /**
  * Comment Dto
  */
+@Getter
+@Setter
 public class CommentDto {
+    private Long commentId;
+    private String content;
+    private String boardType;
+    private Long boardId;
+    private String authorType;
+    private String authorId;
+    private Timestamp createdAt;
+    private Timestamp editedAt;
+    private String adminName; // 댓글에 필요한 authorName
+    private String memberName; // 댓글에 필요한 authorName
 }
