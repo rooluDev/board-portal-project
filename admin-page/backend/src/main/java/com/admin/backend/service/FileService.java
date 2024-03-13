8ffd6d2cdac8975b54d8dc7fea635108040ce0cc
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-13 22:01:00 +0900

    feat : getFileListByBoardId, getFileById 메소드 추가

--- admin-page/backend/src/main/java/com/admin/backend/service/FileService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/FileService.java
@@ -4,6 +4,7 @@ import com.admin.backend.dto.FileDto;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * File Service Interface
@@ -19,4 +20,22 @@ public interface FileService {
      * @return
      */
     List<FileDto> addFile(MultipartFile[] fileList, String boardType, Long boardId);
+
+    /**
+     * boardType에 등록된 board 내 파일 가져오기
+     *
+     * @param boardId
+     * @param boardType
+     * @return
+     */
+    List<FileDto> getFileListByBoardId(Long boardId, String boardType);
+
+    /**
+     * fileId로 File 가져오기
+     *
+     * @param fileId
+     * @return
+     */
+    Optional<FileDto> getFileById(Long fileId);
+
 }
