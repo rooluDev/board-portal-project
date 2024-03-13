4c098e59535eaeec23849653f6ed57579369a940
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-13 22:00:42 +0900

    feat : selectFileListByBoardId, selectFileById 메소드 추가

--- admin-page/backend/src/main/java/com/admin/backend/mapper/FileMapper.java
+++ admin-page/backend/src/main/java/com/admin/backend/mapper/FileMapper.java
@@ -2,6 +2,10 @@ package com.admin.backend.mapper;
 
 import com.admin.backend.dto.FileDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * tb_file DB Mapper
@@ -15,4 +19,21 @@ public interface FileMapper {
      * @param fileDto
      */
     void insertFile(FileDto fileDto);
+
+    /**
+     * tb_file DB SELECT By BoardId And BoardType
+     *
+     * @param boardId
+     * @param boardType
+     * @return
+     */
+    List<FileDto> selectFileListByBoardId(@Param("boardId") Long boardId, @Param("boardType") String boardType);
+
+    /**
+     * SELECT tb_file By FileId
+     *
+     * @param fileId
+     * @return
+     */
+    Optional<FileDto> selectFileById(Long fileId);
 }
