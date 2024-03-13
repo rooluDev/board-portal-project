6c3b124237f31c897a114795d5ace615e4c35255
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-13 21:59:52 +0900

    feat : getCommentListByBoardId, deleteCommentById, addComment, getCommentById 추가

--- admin-page/backend/src/main/java/com/admin/backend/service/CommentService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/CommentService.java
@@ -1,10 +1,43 @@
 package com.admin.backend.service;
 
-import org.springframework.stereotype.Service;
+import com.admin.backend.dto.CommentDto;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Comment Service Interface
  */
-@Service
 public interface CommentService {
+
+    /**
+     * boardType에 맞는 boardId에 있는 댓글 리스트 가져오기
+     *
+     * @param boardId
+     * @param boardType
+     * @return
+     */
+    List<CommentDto> getCommentListByBoardId(Long boardId, String boardType);
+
+    /**
+     * comment 삭제
+     *
+     * @param commentId
+     */
+    void deleteCommentById(Long commentId);
+
+    /**
+     * comment 등록
+     *
+     * @param commentDto
+     */
+    Long addComment(CommentDto commentDto);
+
+    /**
+     * comment 가져오기
+     *
+     * @param commentId
+     * @return
+     */
+    Optional<CommentDto> getCommentById(Long commentId);
 }
