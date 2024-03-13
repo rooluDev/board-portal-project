2c9dfe5761bef5312f2ed373daa6fe4ef6189f7e
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-13 22:06:00 +0900

    feat : getBoardById, deleteBoard, increaseView 메소드 추가

--- admin-page/backend/src/main/java/com/admin/backend/service/FreeBoardService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/FreeBoardService.java
@@ -4,6 +4,7 @@ import com.admin.backend.dto.FreeBoardDto;
 import com.admin.backend.dto.SearchConditionDto;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Free Board Service Interface
@@ -32,4 +33,27 @@ public interface FreeBoardService {
      * @param freeBoardDto
      */
     Long addBoard(FreeBoardDto freeBoardDto);
+
+    /**
+     * 자유게시물 찾기
+     *
+     * @param boardId
+     * @return
+     */
+    Optional<FreeBoardDto> getBoardById(Long boardId);
+
+    /**
+     * 자유게시물 삭제
+     *
+     * @param boardId
+     */
+    void deleteBoard(Long boardId);
+
+    /**
+     * 조회수 증가
+     *
+     * @param boardId
+     */
+    void increaseView(Long boardId);
+
 }
