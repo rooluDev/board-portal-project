3788339dd43bb8b559f60ac6971095e2e53fb14c
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-14 19:51:51 +0900

    init : inquiry_board service Impl

---
+++ admin-page/backend/src/main/java/com/admin/backend/service/InquiryBoardServiceImpl.java
@@ -0,0 +1,14 @@
+package com.admin.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+/**
+ * Inquiry Board Service 구현체
+ */
+@Service
+@Primary
+@RequiredArgsConstructor
+public class InquiryBoardServiceImpl implements InquiryBoardService{
+}
