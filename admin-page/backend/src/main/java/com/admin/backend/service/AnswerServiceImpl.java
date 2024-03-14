32bc4734ca1e7b05a34f64571e1516e79d6f8a92
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-14 19:49:57 +0900

    init : Answer Interface impl

---
+++ admin-page/backend/src/main/java/com/admin/backend/service/AnswerServiceImpl.java
@@ -0,0 +1,14 @@
+package com.admin.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+/**
+ * Answer Service 구현체
+ */
+@Service
+@Primary
+@RequiredArgsConstructor
+public class AnswerServiceImpl implements AnswerService{
+}
