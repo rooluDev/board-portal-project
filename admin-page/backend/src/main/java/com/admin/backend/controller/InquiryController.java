e102c2737a97d5c6b370b2c733f0125816ec13db
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-14 19:52:00 +0900

    init : inquiry controller

---
+++ admin-page/backend/src/main/java/com/admin/backend/controller/InquiryController.java
@@ -0,0 +1,14 @@
+package com.admin.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Inquiry Controller
+ */
+@Controller
+@RequiredArgsConstructor
+@RequestMapping
+public class InquiryController {
+}
