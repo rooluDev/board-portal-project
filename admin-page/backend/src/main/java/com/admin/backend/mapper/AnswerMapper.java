49880a7cdb1b335809ded4cc805deb4f87b1382a
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-15 15:39:54 +0900

    feat : selectByBoardId, insertAnswer, deleteByBoardId 선언

--- admin-page/backend/src/main/java/com/admin/backend/mapper/AnswerMapper.java
+++ admin-page/backend/src/main/java/com/admin/backend/mapper/AnswerMapper.java
@@ -1,5 +1,6 @@
 package com.admin.backend.mapper;
 
+import com.admin.backend.dto.AnswerDto;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -8,4 +9,25 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface AnswerMapper {
 
+    /**
+     * SELECT tb_answer by boardId
+     *
+     * @param boardId
+     * @return
+     */
+    AnswerDto selectByBoardId(Long boardId);
+
+    /**
+     * INSERT tb_answer
+     *
+     * @param answerDto
+     */
+    void insertAnswer(AnswerDto answerDto);
+
+    /**
+     * DELETE tb_answer by BoardId
+     *
+     * @param BoardId
+     */
+    void deleteByBoardId(Long BoardId);
 }
