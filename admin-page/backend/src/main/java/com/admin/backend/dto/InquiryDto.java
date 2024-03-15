49fb358eae4e91396a45944771e70532b9f001f7
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-15 15:33:02 +0900

    feat : Inquiry Dto 멤버변수 추가

--- admin-page/backend/src/main/java/com/admin/backend/dto/InquiryDto.java
+++ admin-page/backend/src/main/java/com/admin/backend/dto/InquiryDto.java
@@ -1,7 +1,23 @@
 package com.admin.backend.dto;
 
+import lombok.*;
+
+import java.sql.Timestamp;
+
 /**
  * Inquiry Dto
  */
+@Getter
+@Setter
 public class InquiryDto {
+    private Long boardId;
+    private String authorId;
+    private String title;
+    private String content;
+    private int views;
+    private String secret;
+    private Timestamp createdAt;
+    private Timestamp editedAt;
+    private String memberName; // 리스트 페이지에 필요한 등록자 이름
+    private String answerId; // 리스트 페이지에 필요한 답변완료 옵션
 }
