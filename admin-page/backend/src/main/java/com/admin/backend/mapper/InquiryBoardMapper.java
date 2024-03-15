f29492543eecb2112ddefc17a08f0d71c7ada068
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-15 15:35:12 +0900

    feat : selectBoardListByCondition, selectTotalRowCountByCondition, selectBoardById, deleteBoardById 생성

--- admin-page/backend/src/main/java/com/admin/backend/mapper/InquiryBoardMapper.java
+++ admin-page/backend/src/main/java/com/admin/backend/mapper/InquiryBoardMapper.java
@@ -1,10 +1,46 @@
 package com.admin.backend.mapper;
 
+import com.admin.backend.dto.InquiryDto;
+import com.admin.backend.dto.SearchConditionDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * tb_inquiry board mapper
  */
 @Mapper
 public interface InquiryBoardMapper {
+
+    /**
+     * 검색조건과 페이지네이션에 맞는 문의 게시물 리스트 SELECT
+     *
+     * @param searchConditionDto
+     * @return
+     */
+    List<InquiryDto> selectBoardListByCondition(SearchConditionDto searchConditionDto);
+
+    /**
+     * 검색조건에 맞는 문의 게시물 총 개수 SELECT
+     *
+     * @param searchConditionDto
+     * @return
+     */
+    int selectTotalRowCountByCondition(SearchConditionDto searchConditionDto);
+
+    /**
+     * boardId(pk)로 board SELECT
+     *
+     * @param boardId
+     * @return
+     */
+    Optional<InquiryDto> selectBoardById(Long boardId);
+
+    /**
+     * DELETE Board By Id
+     *
+     * @param boardId
+     */
+    void deleteBoardById(Long boardId);
 }
