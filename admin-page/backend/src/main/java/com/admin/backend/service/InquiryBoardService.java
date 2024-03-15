edd78616c6e7c5fda842d6ea17724e5f7e4386de
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-15 15:34:40 +0900

    feat : getTotalRowCountByCondition, getBoardListByCondition, getBoardById,deleteBoardById 선언

--- admin-page/backend/src/main/java/com/admin/backend/service/InquiryBoardService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/InquiryBoardService.java
@@ -1,7 +1,44 @@
 package com.admin.backend.service;
 
+import com.admin.backend.dto.InquiryDto;
+import com.admin.backend.dto.SearchConditionDto;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Inquiry Board Service Interface
  */
 public interface InquiryBoardService {
+
+    /**
+     * 검색조건에 맞는 문의 게시물 총 개수 가져오기
+     *
+     * @param searchConditionDto
+     * @return
+     */
+    int getTotalRowCountByCondition(SearchConditionDto searchConditionDto);
+
+    /**
+     * 검색조건과 페이지네이션에 맞는 문의 게시물 리스트 가져오기
+     *
+     * @param searchConditionDto
+     * @return
+     */
+    List<InquiryDto> getBoardListByCondition(SearchConditionDto searchConditionDto);
+
+    /**
+     * 단일 문의 게시물 찾기
+     *
+     * @param boardId
+     * @return
+     */
+    Optional<InquiryDto> getBoardById(Long boardId);
+
+    /**
+     * 게시물 삭제
+     *
+     * @param boardId
+     */
+    void deleteBoardById(Long boardId);
 }
