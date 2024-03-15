fc2850ebb978879578cc2f9e175162d74d1f70ed
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-15 15:39:19 +0900

    feat : Answer Dto 멤버변수 추가

--- admin-page/backend/src/main/java/com/admin/backend/dto/AnswerDto.java
+++ admin-page/backend/src/main/java/com/admin/backend/dto/AnswerDto.java
@@ -1,7 +1,22 @@
 package com.admin.backend.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
 /**
  * Answer Dto
  */
+@Getter
+@Setter
+@Builder
 public class AnswerDto {
+    private Long answerId;
+    private Long boardId;
+    private String authorId;
+    private String content;
+    private Timestamp createdAt;
+    private Timestamp editedAt;
 }
