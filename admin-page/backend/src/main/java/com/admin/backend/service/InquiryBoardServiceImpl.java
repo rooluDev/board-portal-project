d3cc1297082852f92a4174776521fbb25ab0ae12
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-15 15:34:35 +0900

    feat : getTotalRowCountByCondition, getBoardListByCondition, getBoardById,deleteBoardById 구현

--- admin-page/backend/src/main/java/com/admin/backend/service/InquiryBoardServiceImpl.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/InquiryBoardServiceImpl.java
@@ -1,8 +1,14 @@
 package com.admin.backend.service;
 
+import com.admin.backend.dto.InquiryDto;
+import com.admin.backend.dto.SearchConditionDto;
+import com.admin.backend.mapper.InquiryBoardMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Inquiry Board Service 구현체
@@ -11,4 +17,26 @@ import org.springframework.stereotype.Service;
 @Primary
 @RequiredArgsConstructor
 public class InquiryBoardServiceImpl implements InquiryBoardService{
+
+    private final InquiryBoardMapper inquiryBoardMapper;
+
+    @Override
+    public int getTotalRowCountByCondition(SearchConditionDto searchConditionDto) {
+        return inquiryBoardMapper.selectTotalRowCountByCondition(searchConditionDto);
+    }
+
+    @Override
+    public List<InquiryDto> getBoardListByCondition(SearchConditionDto searchConditionDto) {
+        return inquiryBoardMapper.selectBoardListByCondition(searchConditionDto);
+    }
+
+    @Override
+    public Optional<InquiryDto> getBoardById(Long boardId) {
+        return inquiryBoardMapper.selectBoardById(boardId);
+    }
+
+    @Override
+    public void deleteBoardById(Long boardId) {
+        inquiryBoardMapper.deleteBoardById(boardId);
+    }
 }
