13f638c9af88de51572fa1396e169caba2c97b37
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-15 15:38:56 +0900

    feat : getAnswerByBoardId, addAnswer, deleteAnswer 선언

--- admin-page/backend/src/main/java/com/admin/backend/service/AnswerService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/AnswerService.java
@@ -1,7 +1,32 @@
 package com.admin.backend.service;
 
+import com.admin.backend.dto.AnswerDto;
+
+
 /**
  * Answer Service Interface
  */
 public interface AnswerService {
+
+    /**
+     * boardId에 등록된 answer 가져오기
+     *
+     * @param boardId
+     * @return
+     */
+    AnswerDto getAnswerByBoardId(Long boardId);
+
+    /**
+     * 답변 추가
+     *
+     * @param answerDto
+     */
+    void addAnswer(AnswerDto answerDto);
+
+    /**
+     * 답변 삭제
+     *
+     * @param boardId
+     */
+    void deleteAnswer(Long boardId);
 }
