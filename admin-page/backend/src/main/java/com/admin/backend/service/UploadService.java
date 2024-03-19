d5e7135ece8b590d7b9d73c49de479a037e8085f
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-20 00:26:43 +0900

    docs : param

--- admin-page/backend/src/main/java/com/admin/backend/service/UploadService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/UploadService.java
@@ -14,7 +14,7 @@ public interface UploadService {
     /**
      * file을 물리적 파일로 업로드 하는 메소드
      *
-     * @param fileDtoList
+     * @param fileDtoList DB에 저장된 File 리스트
      */
-    void uploadFile(List<FileDto> fileDtoList, MultipartFile[] fileList) throws IOException;
+    void uploadFile(List<FileDto> fileDtoList, MultipartFile[] fileList, String boardType) throws IOException;
 }
