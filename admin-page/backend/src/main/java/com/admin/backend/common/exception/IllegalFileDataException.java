b39fc420af2720d2ca11d484cdb563d7f8353f61
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-20 00:45:24 +0900

    docs : 수정

--- admin-page/backend/src/main/java/com/admin/backend/common/exception/IllegalFileDataException.java
+++ admin-page/backend/src/main/java/com/admin/backend/common/exception/IllegalFileDataException.java
@@ -1,7 +1,7 @@
 package com.admin.backend.common.exception;
 
 /**
- * File 제약사항 어길시 발생하는 Exception
+ * File 업로드 정책을 어길 시 발생하는 Exception
  */
 public class IllegalFileDataException extends RuntimeException{
     public IllegalFileDataException(String message) {
