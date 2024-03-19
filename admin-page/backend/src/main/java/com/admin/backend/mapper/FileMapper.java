a13154eecacaac682b3372c1b4ad6f57097f2e5c
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-20 00:35:45 +0900

    feat : deleteFileById, selectRowCountByBoardId mapper 추가

--- admin-page/backend/src/main/java/com/admin/backend/mapper/FileMapper.java
+++ admin-page/backend/src/main/java/com/admin/backend/mapper/FileMapper.java
@@ -8,32 +8,48 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * tb_file DB Mapper
+ * tb_file Mapper
  */
 @Mapper
 public interface FileMapper {
 
     /**
-     * tb_file DB INSERT
+     * INSERT tb_file
      *
-     * @param fileDto
+     * @param fileDto ( board_type, board_id, original_name, physical_name, file_path, extension, size )
      */
     void insertFile(FileDto fileDto);
 
     /**
-     * tb_file DB SELECT By BoardId And BoardType
+     * SELECT tb_file By BoardId And BoardType
      *
-     * @param boardId
-     * @param boardType
-     * @return
+     * @param boardId   boardId
+     * @param boardType boardType
+     * @return boardType에 boardId의 등록된 파일 리스트
      */
     List<FileDto> selectFileListByBoardId(@Param("boardId") Long boardId, @Param("boardType") String boardType);
 
     /**
      * SELECT tb_file By FileId
      *
-     * @param fileId
-     * @return
+     * @param fileId ( pk )
+     * @return fileId와 일치하는 File
      */
     Optional<FileDto> selectFileById(Long fileId);
+
+    /**
+     * DELETE tb_file By FileId
+     *
+     * @param fileId ( pk )
+     */
+    void deleteFileById(Long fileId);
+
+    /**
+     * SELECT File Row Count by BoardID
+     *
+     * @param boardId   boardId
+     * @param boardType boardType
+     * @return boardType에 boardId의 등록된 파일 리스트의 수
+     */
+    int selectRowCountByBoardId(@Param("boardId") Long boardId, @Param("boardType") String boardType);
 }
