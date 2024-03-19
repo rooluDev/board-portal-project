27828523f4a4f77eadb0f2fce22affc56cf1671c
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-20 00:38:46 +0900

    feat : 멤버변수 추가
    private int deleted;
    private MultipartFile[] file; // 추가 또는 수정 시 필요한 파일 리스트
    private List<Long> deleteFileIdList; // 파일 수정 시 삭제 할 파일 ID에 필요한 리스트
    private int commentCount; // list page에 필요한 댓글 수

--- admin-page/backend/src/main/java/com/admin/backend/dto/FreeBoardDto.java
+++ admin-page/backend/src/main/java/com/admin/backend/dto/FreeBoardDto.java
@@ -1,8 +1,10 @@
 package com.admin.backend.dto;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 /**
  * Free Board Dto
@@ -22,8 +24,12 @@ public class FreeBoardDto {
     private int views;
     private Timestamp createdAt;
     private Timestamp editedAt;
+    private int deleted;
+    private MultipartFile[] file; // 추가 또는 수정 시 필요한 파일 리스트
+    private List<Long> deleteFileIdList; // 파일 수정 시 삭제 할 파일 ID에 필요한 리스트
     private String categoryName; // list page에 필요한 카테고리 이름
     private Long fileId; // list page에 필요한 파일 여부를 위한 파일 아이디
     private String adminName; // list page에 필요한 등록자 이름
     private String memberName; // list page에 필요한 등록자 이름
+    private int commentCount; // list page에 필요한 댓글 수
 }
