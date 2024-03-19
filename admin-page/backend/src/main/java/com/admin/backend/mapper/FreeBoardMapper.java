b45228952e4748b13804e01a8c8716e0a56048b9
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-20 00:39:23 +0900

    docs : param 주석 추가

--- admin-page/backend/src/main/java/com/admin/backend/mapper/FreeBoardMapper.java
+++ admin-page/backend/src/main/java/com/admin/backend/mapper/FreeBoardMapper.java
@@ -8,53 +8,55 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * tb_free_board DB mapper
+ * tb_free_board mapper
  */
 @Mapper
 public interface FreeBoardMapper {
 
     /**
-     * 검색조건에 맞는 자유게시물의 총 개수 SELECT
+     * SELECT totalRowCount By searchCondition
      *
-     * @param searchConditionDto
-     * @return
+     * @param searchConditionDto 검색조건
+     * @return 검색조건 맞는 자유 게시물 리스트 의 수
      */
     int selectTotalRowCountByCondition(SearchConditionDto searchConditionDto);
 
     /**
-     * 검색조건과 페이지네이션에 맞는 자유게시물 리스트 SELECT
+     * SELECT tb_free_board By searchCondition
      *
-     * @param searchConditionDto
-     * @return
+     * @param searchConditionDto 검색조건
+     * @return 검색조건과 페이지네이션에 맞는 자유 게시물 리스트
      */
     List<FreeBoardDto> selectBoardListByCondition(SearchConditionDto searchConditionDto);
 
     /**
-     * 자유 게시물 추가 INSERT
+     * INSERT tb_free_board
      *
-     * @param freeBoardDto
+     * @param freeBoardDto ( category_id, author_type, author_id, title, content )
      */
     void insertBoard(FreeBoardDto freeBoardDto);
 
     /**
-     * 단일 자유 게시물 찾기 SELECT
+     * SELECT tb_free_board By Id
      *
-     * @param boardId
-     * @return
+     * @param boardId ( pk )
+     * @return boardId와 일치하는 tb_free_board
      */
     Optional<FreeBoardDto> selectBoardById(Long boardId);
 
     /**
-     * content = 삭제된 게시판입니다.
+     * UPDATE tb_free_board
+     * SET content = '삭제된 게시물입니다.', deleted = 1
      *
-     * @param boardId
+     * @param boardId ( pk )
      */
-    void updateBoardById(Long boardId);
+    void updateBoardByIdForDelete(Long boardId);
 
     /**
-     * view + 1
+     * UPDATE tb_free_board
+     * SET views = views + 1
      *
-     * @param boardId
+     * @param boardId ( pk )
      */
     void updateView(Long boardId);
 
