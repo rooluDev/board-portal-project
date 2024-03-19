cde25f29cee0b0c42e863151506dfe06058d1458
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-20 00:44:01 +0900

    feat : getTotalRowCountByCondition, getBoardListByCondition, addBoard, getBoardById, deleteBoard, increaseView, modifyBoard 구현

--- admin-page/backend/src/main/java/com/admin/backend/service/GalleryBoardServiceImpl.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/GalleryBoardServiceImpl.java
@@ -1,9 +1,14 @@
 package com.admin.backend.service;
 
+import com.admin.backend.dto.GalleryBoardDto;
+import com.admin.backend.dto.SearchConditionDto;
 import com.admin.backend.mapper.GalleryBoardMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Gallery Board Service 구현체
@@ -14,4 +19,40 @@ import org.springframework.stereotype.Service;
 public class GalleryBoardServiceImpl implements GalleryBoardService{
 
     private final GalleryBoardMapper galleryBoardMapper;
+
+    @Override
+    public int getTotalRowCountByCondition(SearchConditionDto searchConditionDto) {
+        return galleryBoardMapper.selectTotalRowCountByCondition(searchConditionDto);
+    }
+
+    @Override
+    public List<GalleryBoardDto> getBoardListByCondition(SearchConditionDto searchConditionDto) {
+        return galleryBoardMapper.selectBoardListByCondition(searchConditionDto);
+    }
+
+    @Override
+    public Long addBoard(GalleryBoardDto galleryBoardDto) {
+        galleryBoardMapper.insertBoard(galleryBoardDto);
+        return galleryBoardDto.getBoardId();
+    }
+
+    @Override
+    public Optional<GalleryBoardDto> getBoardById(Long boardId) {
+        return galleryBoardMapper.selectBoardById(boardId);
+    }
+
+    @Override
+    public void deleteBoard(Long boardId) {
+        galleryBoardMapper.updateBoardByIdForDelete(boardId);
+    }
+
+    @Override
+    public void increaseView(Long boardId) {
+        galleryBoardMapper.updateView(boardId);
+    }
+
+    @Override
+    public void modifyBoard(GalleryBoardDto galleryBoardDto) {
+        galleryBoardMapper.updateBoard(galleryBoardDto);
+    }
 }
