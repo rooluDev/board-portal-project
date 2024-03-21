3b5e327dd7945e96bca2f49c1e09596e47cda7fe
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-03-21 21:51:39 +0900

    init

--- user-page/frontend/vue.config.js
+++ user-page/frontend/vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  lintOnSave: false,
+
+  outputDir: './build',
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081/',
+        changeOrigin: true,
+      },
+    },
+  },
+});
