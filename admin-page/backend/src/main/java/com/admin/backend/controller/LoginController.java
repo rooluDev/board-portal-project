bb12ce0a67f9bc173abec5b1daffbce30bcbb1db
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-04-03 21:03:30 +0900

    refactor : login logout info 로그 찍기

--- admin-page/backend/src/main/java/com/admin/backend/controller/LoginController.java
+++ admin-page/backend/src/main/java/com/admin/backend/controller/LoginController.java
@@ -11,14 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 /**
  * Login Controller
  */
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/admin")
 @Slf4j
 public class LoginController {
 
@@ -39,36 +38,38 @@ public class LoginController {
     /**
      * 로그인 진행
      *
-     * @param adminDto login form data ( ID, PW )
+     * @param adminDto           login form data ( ID, PW )
      * @param httpServletRequest HttpServletRequest
-     * @return redirect:/admin/board/notice
+     * @return redirect:/board/notice
      */
     @PostMapping("/login")
     public String loginProc(@ModelAttribute AdminDto adminDto, HttpServletRequest httpServletRequest) {
 
         // 로그인 검증
-        // method가 객체를 매개변수로 받아 명확하지 않음. 파라매터로 ID, PW 보내는게 좀 더 명확해 보임
         AdminDto admin = adminService.findAdmin(adminDto.getAdminId(), adminDto.getPassword())
                 .orElseThrow(() -> new LoginFailException("회원 정보가 일치하지 않습니다."));
 
         // 세션 생성
         HttpSession session = httpServletRequest.getSession(true);
+        log.info("Admin Id : " + admin.getAdminId() + " Login");
         session.setAttribute(ADMIN_SESSION_ID, admin);
 
         // 시간 설정
         session.setMaxInactiveInterval(SESSION_TIME);
 
-        return "redirect:/admin/board/notice";
+        return "redirect:/board/notice";
     }
 
     /**
      * 로그아웃 진행
      *
-     * @param request HttpServletRequest
-     * @return redirect:/admin/login
+     * @param request  HttpServletRequest
+     * @param adminDto 세션에 들어이있는 정보
+     * @return redirect:/login
      */
     @GetMapping("/logout")
-    public String logoutProc(HttpServletRequest request) {
+    public String logoutProc(HttpServletRequest request,
+                             @SessionAttribute(name = ADMIN_SESSION_ID) AdminDto adminDto) {
 
         // 세션 가져오기
         HttpSession session = request.getSession(false);
@@ -76,8 +77,9 @@ public class LoginController {
         if (session != null) {
             // 세션 파기
             session.invalidate();
+            log.info("Admin Id : " + adminDto.getAdminId() + " Logout");
         }
 
-        return "redirect:/admin/login";
+        return "redirect:/login";
     }
 }
