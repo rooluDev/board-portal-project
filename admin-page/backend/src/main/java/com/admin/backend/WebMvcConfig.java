524bd1891d41f9e17bef2619a26448fb0fbcdb24
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-04-04 02:39:08 +0900

    feat : WebMvcConfig 설정
    interceptor 와 resourceHandler 설정

---
+++ admin-page/backend/src/main/java/com/admin/backend/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package com.admin.backend;
+
+import com.admin.backend.common.interceptor.InterceptorHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Interceptor Configuration
+ */
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final InterceptorHandler interceptorHandler;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // /login을 제외한 /으로 시작한 모든 uri 인터셉터 설정
+        registry.addInterceptor(interceptorHandler)
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/free/**")
+                .addResourceLocations("file:///Users/user/upload/free/");
+
+        registry.addResourceHandler("/upload/gallery/**")
+                .addResourceLocations("file:///Users/user/upload/gallery/");
+    }
+}
