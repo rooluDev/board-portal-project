d75be979f41d837da2347960feb8fb64f43444db
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-04-04 02:51:32 +0900

    refactor : 검색 조건을 위한 SearchCondition 매개변수 추가

--- admin-page/backend/src/main/java/com/admin/backend/controller/AnswerController.java
+++ admin-page/backend/src/main/java/com/admin/backend/controller/AnswerController.java
@@ -1,18 +1,20 @@
 package com.admin.backend.controller;
 
+import com.admin.backend.common.utils.StringUtils;
 import com.admin.backend.common.validator.AnswerValidator;
 import com.admin.backend.dto.AdminDto;
 import com.admin.backend.dto.AnswerDto;
+import com.admin.backend.dto.SearchConditionDto;
 import com.admin.backend.service.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Answer Controller
  */
 @Controller
-@RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AnswerController {
 
@@ -21,18 +23,23 @@ public class AnswerController {
     /**
      * 답변 등록
      *
-     * @param boardId  PathVariable
-     * @param adminDto 저장된 세션 정보 ( table column에 저장될 데이터 )
-     * @param content  답변 내용
-     * @return redirect:/admin/board/inquiry
+     * @param boardId            PathVariable
+     * @param adminDto           저장된 세션 정보 ( table column에 저장될 데이터 )
+     * @param content            답변 내용
+     * @param searchConditionDto 등록 후 검색조건을 유지하기 위한 쿼리스트링
+     * @param redirectAttributes 등록 완료 alert를 위한 redirectAttributes
+     * @return redirect:/board/inquiry
      */
     @PostMapping("/answer/{boardId}")
     public String addAnswer(@PathVariable(name = "boardId") Long boardId,
                             @SessionAttribute(name = LoginController.ADMIN_SESSION_ID) AdminDto adminDto,
-                            @RequestParam(name = "content") String content) {
+                            @RequestParam(name = "content") String content,
+                            @ModelAttribute SearchConditionDto searchConditionDto,
+                            RedirectAttributes redirectAttributes) {
 
         // 유효성 검증
-        AnswerValidator.validateAnswer(content);
+        // TODO : 유효성 검증 및 검색조건 유지
+//        AnswerValidator.validateAnswer(content);
 
         AnswerDto answerDto = AnswerDto.builder()
                 .boardId(boardId)
@@ -47,6 +54,9 @@ public class AnswerController {
             answerService.addAnswer(answerDto);
         }
 
-        return "redirect:/admin/board/inquiry";
+        // 등록 완료 응답 값 설정
+        redirectAttributes.addFlashAttribute("write", 1);
+
+        return "redirect:/board/inquiry" + StringUtils.searchConditionToQueryStringWithOutCategory(searchConditionDto);
     }
 }
