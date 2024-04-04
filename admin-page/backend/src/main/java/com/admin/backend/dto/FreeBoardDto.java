61eaa321e49b94a9d3c5647e0ff38c6de3ae9455
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-04-04 20:03:57 +0900

    refactor : file, deleteFileIdList 삭제

--- admin-page/backend/src/main/java/com/admin/backend/dto/FreeBoardDto.java
+++ admin-page/backend/src/main/java/com/admin/backend/dto/FreeBoardDto.java
@@ -22,11 +22,9 @@ public class FreeBoardDto {
     private String title;
     private String content;
     private int views;
+    private int deleted;
     private Timestamp createdAt;
     private Timestamp editedAt;
-    private int deleted;
-    private MultipartFile[] file; // 추가 또는 수정 시 필요한 파일 리스트
-    private List<Long> deleteFileIdList; // 파일 수정 시 삭제 할 파일 ID에 필요한 리스트
     private String categoryName; // list page에 필요한 카테고리 이름
     private Long fileId; // list page에 필요한 파일 여부를 위한 파일 아이디
     private String adminName; // list page에 필요한 등록자 이름
