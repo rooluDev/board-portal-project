feaf88065e3f4b9c5e43015758095cccdb5c158c
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-04-13 17:04:48 +0900

    feat : Board Title Component

---
+++ user-page/frontend/src/components/BoardTitleBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot name="my-inquiry"></slot>
+    <slot name="board-category"></slot>
+    <slot name="more"></slot>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
