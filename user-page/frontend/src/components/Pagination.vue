c89efa5c352c8504f0136f624631ec491275272e
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-04-13 17:10:48 +0900

    feat : Pagination Component

---
+++ user-page/frontend/src/components/Pagination.vue
@@ -0,0 +1,34 @@
+<template>
+  <li v-for="page in totalPageNum" :key="page" style="display: inline-flex">
+    <span @click="getBoardList(page)">{{ page }}</span>
+  </li>
+</template>
+
+<script>
+import {getCurrentInstance} from "vue";
+
+export default {
+  props: {
+    totalPageNum: {
+      type: Number,
+      required: false
+    }
+  },
+  emits: ['click'],
+  setup() {
+    const {emit} = getCurrentInstance();
+
+    const getBoardList = (page) => {
+      emit('click', page);
+    }
+
+    return {
+      getBoardList
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
