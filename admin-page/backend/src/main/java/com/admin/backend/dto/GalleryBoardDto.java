c44b75ca058706f106d3722027fec5ce2ff1f6b0
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 05:18:09 +0900

    feat : valid 어노테이션 추가

--- admin-page/backend/src/main/java/com/admin/backend/dto/GalleryBoardDto.java
+++ admin-page/backend/src/main/java/com/admin/backend/dto/GalleryBoardDto.java
@@ -1,5 +1,7 @@
 package com.admin.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,23 +14,23 @@ import java.util.List;
  */
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Slf4j
 public class GalleryBoardDto {
     private Long boardId;
     private Long categoryId;
     private String authorType;
     private String authorId;
+    @NotBlank(message = "제목을 입력하세요.")
+    @Size(max = 99, message = "제목을 100자 미만으로 작성하세요")
     private String title;
+    @NotBlank(message = "내용을 입력하세요.")
+    @Size(max = 3999, message = "내용을 4000자 미만으로 작성하세요.")
     private String content;
     private int views;
+    private int isDeleted;
     private Timestamp createdAt;
     private Timestamp editedAt;
-    private int deleted;
-    private MultipartFile[] file;
-    private List<Long> deleteFileIdList;
     private String categoryName; // list page에 필요한 카테고리 이름
     private String filePath; // list page 이미지 추출 시 필요한 파일 경로
     private String physicalName; // list page 이미지 추출 시 필요한 저장된 파일 이름
