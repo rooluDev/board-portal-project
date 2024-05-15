e0ba351cab758ee20065bd1cd585035ddffa4510
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:31:34 +0900

    feat : NoticeBoardController 추가

---
+++ user-page/backend/src/main/java/com/user/backend/controller/NoticeBoardController.java
@@ -0,0 +1,95 @@
+package com.user.backend.controller;
+
+
+import com.user.backend.common.exception.custom.BoardNotFoundException;
+import com.user.backend.common.exception.response.ErrorCode;
+import com.user.backend.common.type.Board;
+import com.user.backend.common.utils.PaginationUtils;
+import com.user.backend.dto.CategoryDto;
+import com.user.backend.dto.NoticeBoardDto;
+import com.user.backend.dto.SearchConditionDto;
+import com.user.backend.service.CategoryService;
+import com.user.backend.service.NoticeBoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Notice Board Controller
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class NoticeBoardController {
+
+    private final NoticeBoardService noticeBoardService;
+    private final CategoryService categoryService;
+
+    /**
+     * 검색조건에 따른 공지사항 게시판 데이터 GET
+     *
+     * @param searchConditionDto 검색조건
+     * @return {
+     * totalPageNUm: 0,
+     * noticeBoardList: [],
+     * fixedNoticeBoardList: [],
+     * cateogryList: [],
+     * searchCondition: {}
+     * }
+     */
+    @GetMapping("/boards/notice")
+    public ResponseEntity<Map> getBoardList(@ModelAttribute SearchConditionDto searchConditionDto) {
+
+        List<NoticeBoardDto> noticeBoardList = noticeBoardService.getBoardListByCondition(searchConditionDto);
+        List<NoticeBoardDto> fixedNoticeBoardList = noticeBoardService.getFixedBoardList();
+        List<CategoryDto> categoryList = categoryService.getCategoryListByBoardType(Board.NOTICE_BOARD.getBoardType());
+
+        // 페이지네이션 설정
+        int totalRowCount = noticeBoardService.getTotalRowCountByCondition(searchConditionDto);
+        int totalPageNum = PaginationUtils.getTotalPageNum(totalRowCount, searchConditionDto.getPageSize());
+
+        Map response = new HashMap();
+        response.put("totalPageNum", totalPageNum);
+        response.put("noticeBoardList", noticeBoardList);
+        response.put("fixedNoticeBoardList", fixedNoticeBoardList);
+        response.put("categoryList", categoryList);
+        response.put("searchCondition", searchConditionDto);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    /**
+     * 공지사항 게시판 보기 페이지에 필요한 데이터
+     *
+     * @param boardId PathVariable ( pk )
+     * @return noticeBoard
+     */
+    @GetMapping("/board/notice/{boardId}")
+    public ResponseEntity<NoticeBoardDto> getBoard(@PathVariable(name = "boardId") Long boardId) {
+
+        NoticeBoardDto noticeBoardDto = noticeBoardService.getBoardByBoardId(boardId).orElseThrow(() -> new BoardNotFoundException(ErrorCode.BOARD_NOT_FOUND));
+
+        return ResponseEntity.ok().body(noticeBoardDto);
+    }
+
+    /**
+     * 게시판 조회수 1 증가
+     *
+     * @param boardId PathVariable ( pk )
+     * @return null
+     */
+    @PatchMapping("/board/notice/{boardId}/increase-view")
+    public ResponseEntity increaseView(@PathVariable(name = "boardId") Long boardId) {
+
+        noticeBoardService.getBoardByBoardId(boardId).orElseThrow(() -> new BoardNotFoundException(ErrorCode.BOARD_NOT_FOUND));
+        // 조회수 증가
+        noticeBoardService.increaseView(boardId);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
