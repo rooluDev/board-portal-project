009b81c4ed6c1eecee7f7c0d0fa7a8806a2f74d8
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 05:14:29 +0900

    feat : storageThumbnail, deleteFileList 추가

--- admin-page/backend/src/main/java/com/admin/backend/service/FileStorageService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/FileStorageService.java
@@ -1,8 +1,10 @@
 package com.admin.backend.service;
 
+import com.admin.backend.dto.FileDto;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * FileStorageService Interface
@@ -15,7 +17,22 @@ public interface FileStorageService {
      * @param fileList  저장할 파일리스트
      * @param boardId   bardId ( pk )
      * @param boardType boardType
-     * @throws IOException IOException
+     * @param thumbnail 썸네일 저장 할지
      */
-    void fileStorage(MultipartFile[] fileList, Long boardId, String boardType) throws IOException;
+    void storageFileList(MultipartFile[] fileList, Long boardId, String boardType, boolean thumbnail);
+
+    /**
+     * 썸네일 DB저장 및 물리적 파일 저장
+     *
+     * @param fileDto 썸네일로 저장할 FileDto
+     */
+    void storageThumbnail(FileDto fileDto);
+
+    /**
+     * 파일 리스트 삭제
+     *
+     * @param deleteFileIdList 삭제할 파일들의 pk 리스트
+     * @return 썸네일로 만든 파일 대상이 삭제가 되었는지
+     */
+    boolean deleteFileList(List<Long> deleteFileIdList);
 }
