65678b10b5391ea09a52286a45d054389350fbbc
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:50:39 +0900

    feat : 에러페이지 추가

---
+++ user-page/frontend/src/pages/Error.vue
@@ -0,0 +1,26 @@
+<template>
+  <h3>잘못된 요청입니다</h3>
+  <button type="button" @click="goToMain">메인 페이지</button>
+</template>
+<script>
+import {useRouter} from "vue-router";
+
+export default {
+  setup() {
+    const router = useRouter();
+
+    const goToMain = () => {
+      router.push({
+        name: 'Main'
+      })
+    }
+
+    return {
+      goToMain
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
