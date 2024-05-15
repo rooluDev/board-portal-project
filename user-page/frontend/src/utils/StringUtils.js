387c2ff4521a1e42aef106d418d8870da2fba4e9
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:56:07 +0900

    feat : parseToQueryString 추가

--- user-page/frontend/src/utils/StringUtils.js
+++
@@ -1,7 +0,0 @@
-export const truncateTitle = (title, maxLength) => {
-    if (title.length <= maxLength) {
-        return title;
-    } else {
-        return title.substring(0, maxLength) + '...';
-    }
-}
---
+++ user-page/frontend/src/utils/stringUtils.js
@@ -0,0 +1,42 @@
+import {Board} from "@/type/boardType";
+
+/**
+ * 타이틀 일정 길이가 넘어가면 그때부터 ...추가
+ *
+ * @param title 제목
+ * @param maxLength 최대길이
+ * @returns {*|string} ... 추가한 제목
+ */
+export const truncateTitle = (title, maxLength) => {
+    if (title.length <= maxLength) {
+        return title;
+    } else {
+        return title.substring(0, maxLength) + '...';
+    }
+}
+/**
+ * 검색조건 Object를 보드 타입의 맞게 쿼리스트링으로 변환
+ *
+ * @param searchCondition 검색조건
+ * @param boardType 보드 종류
+ * @returns {*|string} 쿼리스트링
+ */
+export const parseToQueryString = (searchCondition, boardType) => {
+    let queryString = '';
+    switch (boardType) {
+        case Board.NOTICE_BOARD:
+        case Board.FREE_BOARD:
+        case Board.GALLERY_BOARD:
+            queryString = `?startDate=${searchCondition.startDate}&endDate=${searchCondition.endDate}` +
+                `&category=${searchCondition.category}&searchText=${searchCondition.searchText}&pageSize=${searchCondition.pageSize}` +
+                `&orderValue=${searchCondition.orderValue}&orderDirection=${searchCondition.orderDirection}&pageNum=${searchCondition.pageNum}`;
+            return queryString;
+
+        case Board.INQUIRY_BOARD:
+            queryString = `?startDate=${searchCondition.startDate}&endDate=${searchCondition.endDate}` +
+                `&searchText=${searchCondition.searchText}&pageSize=${searchCondition.pageSize}&orderValue=${searchCondition.orderValue}` +
+                `&orderDirection=${searchCondition.orderDirection}&pageNum=${searchCondition.pageNum}&my=${searchCondition.my}`;
+            return queryString;
+    }
+
+}
