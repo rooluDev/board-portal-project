c5caadfea042422509671fa0bffac7a03b7d295a
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:31:51 +0900

    refactor : valid 어노테이션 추가

--- user-page/backend/src/main/java/com/user/backend/dto/NoticeBoardDto.java
+++ user-page/backend/src/main/java/com/user/backend/dto/NoticeBoardDto.java
@@ -1,5 +1,7 @@
 package com.user.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,10 +16,14 @@ public class NoticeBoardDto {
     private Long boardId;
     private Long categoryId;
     private String authorId;
+    @NotBlank(message = "제목을 입력하세요.")
+    @Size(max = 99, message = "100자 미만으로 작성하세요")
     private String title;
+    @NotBlank(message = "내용을 입력하세요.")
+    @Size(max = 3999, message = "내용이 4000자 미만으로 작성하세요.")
     private String content;
     private int views;
-    private String fixed;
+    private String isFixed;
     private Timestamp createdAt;
     private Timestamp editedAt;
     private String authorName; // 리스트 페이지에 필요한 등록자 이름
