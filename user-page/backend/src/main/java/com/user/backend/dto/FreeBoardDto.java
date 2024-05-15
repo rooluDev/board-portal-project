e187ddd083b10f304f7d3b8548a83ca7a1e5be88
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:20:42 +0900

    refactor : valid 어노테이션 추가

--- user-page/backend/src/main/java/com/user/backend/dto/FreeBoardDto.java
+++ user-page/backend/src/main/java/com/user/backend/dto/FreeBoardDto.java
@@ -1,10 +1,10 @@
 package com.user.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 /**
  * Free Board Dto
@@ -19,14 +19,16 @@ public class FreeBoardDto {
     private Long categoryId;
     private String authorType;
     private String authorId;
+    @NotBlank(message = "제목을 입력하세요.")
+    @Size(max = 99, message = "100자 미만으로 작성하세요")
     private String title;
+    @NotBlank(message = "내용을 입력하세요.")
+    @Size(max = 3999, message = "내용이 4000자 미만으로 작성하세요.")
     private String content;
     private int views;
     private Timestamp createdAt;
     private Timestamp editedAt;
-    private int deleted;
-    private MultipartFile[] file; // 추가 또는 수정 시 필요한 파일 리스트
-    private List<Long> deleteFileIdList; // 파일 수정 시 삭제 할 파일 ID에 필요한 리스트
+    private int isDeleted;
     private String categoryName; // list page에 필요한 카테고리 이름
     private Long fileId; // list page에 필요한 파일 여부를 위한 파일 아이디
     private String adminName; // list page에 필요한 등록자 이름
