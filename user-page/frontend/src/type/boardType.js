ee4f76985a573add95b81f6dde1b3cdebb09036f
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:48:58 +0900

    feat : boardType.js 추가

---
+++ user-page/frontend/src/type/boardType.js
@@ -0,0 +1,11 @@
+/**
+ * BoardType
+ *
+ * @type {Readonly<{INQUIRY_BOARD: string, FREE_BOARD: string, GALLERY_BOARD: string, NOTICE_BOARD: string}>}
+ */
+export const Board = Object.freeze({
+    FREE_BOARD: "free",
+    NOTICE_BOARD: "notice",
+    GALLERY_BOARD: "gallery",
+    INQUIRY_BOARD: "inquiry"
+});
