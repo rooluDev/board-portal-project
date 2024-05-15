ee8741f8b00c64fb62382d80c87c685be4e7455e
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 05:00:00 +0900

    feat : valid 어노테이션 추가

--- admin-page/backend/src/main/java/com/admin/backend/dto/AnswerDto.java
+++ admin-page/backend/src/main/java/com/admin/backend/dto/AnswerDto.java
@@ -1,5 +1,7 @@
 package com.admin.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.sql.Timestamp;
@@ -16,6 +18,8 @@ public class AnswerDto {
     private Long answerId;
     private Long boardId;
     private String authorId;
+    @NotBlank(message = "답변을 입력하세요.")
+    @Size(max = 3999, message = "답변을 4000자 미만으로 입력하세요.")
     private String content;
     private Timestamp createdAt;
     private Timestamp editedAt;
