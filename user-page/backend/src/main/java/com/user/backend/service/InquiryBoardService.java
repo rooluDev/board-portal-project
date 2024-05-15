7a3f4b325a2a1eaf01523b13b3774679e447d168
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:26:39 +0900

    feat : getBoardListForMain, addBoard, modifyBoard, getBoardByIdAndMemberId 추가

--- user-page/backend/src/main/java/com/user/backend/service/InquiryBoardService.java
+++ user-page/backend/src/main/java/com/user/backend/service/InquiryBoardService.java
@@ -1,6 +1,6 @@
 package com.user.backend.service;
 
-import com.user.backend.dto.InquiryDto;
+import com.user.backend.dto.InquiryBoardDto;
 import com.user.backend.dto.SearchConditionDto;
 
 import java.util.List;
@@ -17,7 +17,7 @@ public interface InquiryBoardService {
      * @param searchConditionDto 검색조건
      * @return 검색조건에 맞는 문의 게시물 총 개수
      */
-    int getTotalRowCountByCondition(SearchConditionDto searchConditionDto);
+    int getTotalRowCountByCondition(SearchConditionDto searchConditionDto, String memberId);
 
     /**
      * 검색조건과 페이지네이션에 맞는 문의 게시물 리스트 가져오기
@@ -25,7 +25,7 @@ public interface InquiryBoardService {
      * @param searchConditionDto
      * @return 검색조건과 페이지네이션에 맞는 문의 게시물 리스트
      */
-    List<InquiryDto> getBoardListByCondition(SearchConditionDto searchConditionDto);
+    List<InquiryBoardDto> getBoardListByCondition(SearchConditionDto searchConditionDto, String memberId);
 
     /**
      * 문의 게시물 찾기
@@ -33,7 +33,7 @@ public interface InquiryBoardService {
      * @param boardId ( pk )
      * @return boardId와 일치하는 문의 게시물
      */
-    Optional<InquiryDto> getBoardById(Long boardId);
+    Optional<InquiryBoardDto> getBoardById(Long boardId);
 
     /**
      * 문의 게시물 삭제
@@ -47,5 +47,35 @@ public interface InquiryBoardService {
      *
      * @param boardId ( pk )
      */
-    void increaseViewById(Long boardId);
+    void increaseView(Long boardId);
+
+    /**
+     * 메인 페이지에 필요한 문의 게시판 리스트 가져오기
+     *
+     * @return 메인 페이지에 필요한 문의 게시판 리스트
+     */
+    List<InquiryBoardDto> getBoardListForMain();
+
+    /**
+     * 문의 게시판 추가
+     *
+     * @param inquiryBoardDto ( author_id, title, content, is_secret )
+     */
+    void addBoard(InquiryBoardDto inquiryBoardDto);
+
+    /**
+     * 문의 게시판 수정
+     *
+     * @param inquiryBoardDto ( title, content, is_secret )
+     */
+    void modifyBoard(InquiryBoardDto inquiryBoardDto);
+
+    /**
+     * boardId와 memberId가 일치하는 게시물 가져오기
+     *
+     * @param boardId ( pk )
+     * @param memberId memberId
+     * @return boardId와 memberId가 일치하는 게시물
+     */
+    Optional<InquiryBoardDto> getBoardByIdAndMemberId(Long boardId, String memberId);
 }
