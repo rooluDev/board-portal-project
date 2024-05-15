3b6ec81b838cf28998f2ec90ce6745c081d5d7f1
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:34:51 +0900

    feat : ThumbnailDto 추가

---
+++ user-page/backend/src/main/java/com/user/backend/dto/ThumbnailDto.java
@@ -0,0 +1,25 @@
+package com.user.backend.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+/**
+ * Thumbnail Dto
+ */
+@Getter
+@Setter
+@Builder
+public class ThumbnailDto {
+    private Long thumbnailId;
+    private Long fileId;
+    private String originalName;
+    private String physicalName;
+    private String filePath;
+    private String extension;
+    private Long size;
+    private Timestamp createdAt;
+    private Timestamp editedAt;
+}
