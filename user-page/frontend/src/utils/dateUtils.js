bd6b1e0b4e621ba3de71802a03a97aa6872fd0b7
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:50:14 +0900

    docs 추가

--- user-page/frontend/src/utils/dateUtils.js
+++ user-page/frontend/src/utils/dateUtils.js
@@ -1,3 +1,10 @@
+/**
+ * newDateLimit보다 작으면 true 크면 false를 반환
+ *
+ * @param createdAt 생성일자
+ * @param newDateLimit 한계점
+ * @returns {boolean} boolean
+ */
 export const isNew = (createdAt, newDateLimit) => {
     const today = new Date();
     const difference = (today - new Date(createdAt)) / (1000 * 60 * 60 * 24);
