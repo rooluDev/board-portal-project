3947642dff39d1bf9fbf8c9a087fc91fbe22b04a
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 05:00:28 +0900

    feat : BoardNotFoundException 기본생성자 추가

--- admin-page/backend/src/main/java/com/admin/backend/common/exception/BoardNotFoundException.java
+++ admin-page/backend/src/main/java/com/admin/backend/common/exception/BoardNotFoundException.java
@@ -4,6 +4,11 @@ package com.admin.backend.common.exception;
  * 요청한 게시물이 없을 경우 발생하는 Exception
  */
 public class BoardNotFoundException extends RuntimeException{
+
+    public BoardNotFoundException() {
+        super();
+    }
+
     public BoardNotFoundException(String message) {
         super(message);
     }
