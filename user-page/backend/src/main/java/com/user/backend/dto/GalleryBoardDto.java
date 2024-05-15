734f1fab7054880187a4e33bc57cbc9440be59cf
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:22:49 +0900

    feat : valid 어노테이션  추가

--- user-page/backend/src/main/java/com/user/backend/dto/GalleryBoardDto.java
+++ user-page/backend/src/main/java/com/user/backend/dto/GalleryBoardDto.java
@@ -1,11 +1,10 @@
 package com.user.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 /**
  * Gallery Board Dto
@@ -15,21 +14,23 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Slf4j
 public class GalleryBoardDto {
     private Long boardId;
     private Long categoryId;
     private String authorType;
     private String authorId;
+    @NotBlank(message = "제목을 입력하세요.")
+    @Size(max = 99, message = "100자 미만으로 작성하세요")
     private String title;
+    @NotBlank(message = "내용을 입력하세요.")
+    @Size(max = 3999, message = "내용이 4000자 미만으로 작성하세요.")
     private String content;
     private int views;
     private Timestamp createdAt;
     private Timestamp editedAt;
-    private int deleted;
-    private MultipartFile[] file;
-    private List<Long> deleteFileIdList;
+    private int isDeleted;
     private String categoryName; // list page에 필요한 카테고리 이름
+    private Long thumbnailId; // list page에 필요한 thumbnailId
     private String filePath; // list page 이미지 추출 시 필요한 파일 경로
     private String physicalName; // list page 이미지 추출 시 필요한 저장된 파일 이름
     private String extension; // list page 이미지 추출 시 필요한 확장자
