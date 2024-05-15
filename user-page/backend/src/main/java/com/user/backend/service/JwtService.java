1ca61b59084a0eb0231ab86aa7c6c9ae2763ab2a
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:28:30 +0900

    feat : JwtService 추가

---
+++ user-page/backend/src/main/java/com/user/backend/service/JwtService.java
@@ -0,0 +1,28 @@
+package com.user.backend.service;
+
+import com.user.backend.dto.MemberDto;
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * Jwt Service
+ */
+public interface JwtService {
+
+    /**
+     * JWT에서 memberId 추출
+     *
+     * @param request HttpServletRequest
+     * @return memberId
+     */
+    String getMemberIdFromToken(HttpServletRequest request);
+
+    /**
+     * JWT 생성
+     *
+     * @param memberDto JWT에 들어갈 정보
+     * @return JWT
+     */
+    String createToken(MemberDto memberDto);
+
+    String getMemberNameFromToken(HttpServletRequest request);
+}
