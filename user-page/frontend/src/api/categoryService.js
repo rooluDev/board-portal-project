a36a0fdad22544c6f9128d486c0ff8a43a9c93bd
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:49:17 +0900

    feat : categoryService.js 추가

--- user-page/frontend/src/api/categoryService.js
+++ user-page/frontend/src/api/categoryService.js
@@ -1,10 +1,13 @@
-import axios from "axios";
+import {api} from "@/api/apiConfig";
 
+/**
+ * GET /api/categories?boardType=
+ * 보드타입에 맞는 카테고리 리스트 가져오기
+ *
+ * @param boardType boardType
+ * @returns {Promise<any>} categoryList
+ */
 export const fetchCategoryListByBoardType = async (boardType) => {
-    try {
-        const res = await axios.get(`/api/categories?boardType=${boardType}`);
-        return res.data;
-    } catch (error){
-        throw new Error();
-    }
+    const res = await api.get(`/categories?boardType=${boardType}`);
+    return res.data;
 }
