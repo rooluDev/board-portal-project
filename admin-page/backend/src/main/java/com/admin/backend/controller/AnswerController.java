ea40cec027606265512942676b1cf2329f061fc4
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:59:48 +0900

    feat : 유효성 검증 추가

--- admin-page/backend/src/main/java/com/admin/backend/controller/AnswerController.java
+++ admin-page/backend/src/main/java/com/admin/backend/controller/AnswerController.java
@@ -1,13 +1,15 @@
 package com.admin.backend.controller;
 
+import com.admin.backend.common.utils.BindingResultUtils;
 import com.admin.backend.common.utils.StringUtils;
-import com.admin.backend.common.validator.AnswerValidator;
 import com.admin.backend.dto.AdminDto;
 import com.admin.backend.dto.AnswerDto;
 import com.admin.backend.dto.SearchConditionDto;
 import com.admin.backend.service.AnswerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -23,32 +25,29 @@ public class AnswerController {
     /**
      * 답변 등록
      *
-     * @param boardId            PathVariable
      * @param adminDto           저장된 세션 정보 ( table column에 저장될 데이터 )
-     * @param content            답변 내용
+     * @param answerDto          답변 내용
      * @param searchConditionDto 등록 후 검색조건을 유지하기 위한 쿼리스트링
      * @param redirectAttributes 등록 완료 alert를 위한 redirectAttributes
      * @return redirect:/board/inquiry
      */
-    @PostMapping("/answer/{boardId}")
-    public String addAnswer(@PathVariable(name = "boardId") Long boardId,
-                            @SessionAttribute(name = LoginController.ADMIN_SESSION_ID) AdminDto adminDto,
-                            @RequestParam(name = "content") String content,
+    @PostMapping("/answer")
+    public String addAnswer(@SessionAttribute(name = LoginController.ADMIN_SESSION_ID) AdminDto adminDto,
+                            @Valid @ModelAttribute AnswerDto answerDto,
+                            BindingResult bindingResult,
                             @ModelAttribute SearchConditionDto searchConditionDto,
                             RedirectAttributes redirectAttributes) {
 
-        // 유효성 검증
-        // TODO : 유효성 검증 및 검색조건 유지
-//        AnswerValidator.validateAnswer(content);
+        if (bindingResult.hasErrors()) {
+            String errorMessage = BindingResultUtils.getErrorMessage(bindingResult, new String[]{"content"});
+            redirectAttributes.addFlashAttribute("errorMessage", errorMessage);
+            return "redirect:/board/notice" + answerDto.getBoardId() + StringUtils.searchConditionToQueryStringWithOutCategory(searchConditionDto);
+        }
 
-        AnswerDto answerDto = AnswerDto.builder()
-                .boardId(boardId)
-                .authorId(adminDto.getAdminId())
-                .content(content)
-                .build();
+        answerDto.setAuthorId(adminDto.getAdminId());
 
         // 답변이 이미 존재 할시 수정 그 외 추가
-        if (answerService.getAnswerByBoardId(boardId).isPresent()) {
+        if (answerService.getAnswerByBoardId(answerDto.getBoardId()).isPresent()) {
             answerService.modifyAnswer(answerDto);
         } else {
             answerService.addAnswer(answerDto);
