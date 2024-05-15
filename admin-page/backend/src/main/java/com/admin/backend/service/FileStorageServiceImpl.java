96e4388b1951f135a9bc2cdc0922f52e75fc41be
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 05:14:35 +0900

    feat : storageThumbnail, deleteFileList 추가

--- admin-page/backend/src/main/java/com/admin/backend/service/FileStorageServiceImpl.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/FileStorageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.admin.backend.service;
 
+import com.admin.backend.common.exception.IllegalFileDataException;
 import com.admin.backend.dto.FileDto;
+import com.admin.backend.dto.ThumbnailDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -15,14 +17,41 @@ import java.util.List;
 @Service
 @Primary
 @RequiredArgsConstructor
-public class FileStorageServiceImpl implements FileStorageService{
+public class FileStorageServiceImpl implements FileStorageService {
 
     private final StorageService storageService;
     private final FileService fileService;
+    private final ThumbnailService thumbnailService;
 
     @Override
-    public void fileStorage(MultipartFile[] fileList, Long boardId, String boardType) throws IOException {
-        List<FileDto> fileDtoList = storageService.storageFiles(fileList, boardType);
-        fileService.addFile(fileDtoList,boardId);
+    public void storageFileList(MultipartFile[] fileList, Long boardId, String boardType, boolean thumbnail) {
+        List<FileDto> fileDtoList = storageService.storageFileList(fileList, boardType);
+        fileService.addFileList(fileDtoList, boardId);
+        if (thumbnail && fileDtoList != null) {
+            ThumbnailDto thumbnailDto = storageService.storageThumbnailFromFile(fileDtoList.get(0));
+            thumbnailDto.setFileId(fileDtoList.get(0).getFileId());
+            thumbnailService.addThumbnail(thumbnailDto);
+        }
+    }
+
+    @Override
+    public void storageThumbnail(FileDto fileDto) {
+        ThumbnailDto thumbnailDto = storageService.storageThumbnailFromFile(fileDto);
+        thumbnailDto.setFileId(fileDto.getFileId());
+        thumbnailService.addThumbnail(thumbnailDto);
+    }
+
+    // TODO : void, boolean 따로?
+    @Override
+    public boolean deleteFileList(List<Long> deleteFileIdList) {
+        boolean isThumbnailDeleted = false;
+        for (Long fileId : deleteFileIdList) {
+            int deletedRow = thumbnailService.deleteThumbnailByFileId(fileId);
+            if (deletedRow == 1) {
+                isThumbnailDeleted = true;
+            }
+        }
+        fileService.deleteFileList(deleteFileIdList);
+        return isThumbnailDeleted;
     }
 }
