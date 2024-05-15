2c5ccf4e2e900f0c6d625c62fee37a7cb4fbf772
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:54:31 +0900

    feat : vuetify 추가

--- user-page/frontend/src/main.js
+++ user-page/frontend/src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '@/router';
-import store from "@/store";
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-const app = createApp(App);
-app.use(router).use(store).mount('#app');
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
