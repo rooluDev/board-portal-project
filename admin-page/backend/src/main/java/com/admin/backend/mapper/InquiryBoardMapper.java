29dd9a61e97ff3a57071a8e90754a8bf3510f083
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 05:19:02 +0900

    rename : InquiryBoardDto

--- admin-page/backend/src/main/java/com/admin/backend/mapper/InquiryBoardMapper.java
+++ admin-page/backend/src/main/java/com/admin/backend/mapper/InquiryBoardMapper.java
@@ -1,6 +1,6 @@
 package com.admin.backend.mapper;
 
-import com.admin.backend.dto.InquiryDto;
+import com.admin.backend.dto.InquiryBoardDto;
 import com.admin.backend.dto.SearchConditionDto;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -19,7 +19,7 @@ public interface InquiryBoardMapper {
      * @param searchConditionDto 검색조건
      * @return 검색조건과 페이지네이션에 맞는 문의 게시물 리스트
      */
-    List<InquiryDto> selectBoardListByCondition(SearchConditionDto searchConditionDto);
+    List<InquiryBoardDto> selectBoardListByCondition(SearchConditionDto searchConditionDto);
 
     /**
      * SELECT totalRowCount By searchCondition
@@ -35,7 +35,7 @@ public interface InquiryBoardMapper {
      * @param boardId ( pk )
      * @return boardId와 일치하는 tb_inquiry_board
      */
-    Optional<InquiryDto> selectBoardById(Long boardId);
+    Optional<InquiryBoardDto> selectBoardById(Long boardId);
 
     /**
      * DELETE tb_inquiry_board By Id
