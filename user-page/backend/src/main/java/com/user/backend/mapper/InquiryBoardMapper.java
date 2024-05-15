39d653c5dbd33ad5584da4b941ddc57d19526188
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-16 04:26:15 +0900

    feat : selectBoardListForMain, insertBoard, updateBoard, selectBoardByIdAndMemberId 추가

--- user-page/backend/src/main/java/com/user/backend/mapper/InquiryBoardMapper.java
+++ user-page/backend/src/main/java/com/user/backend/mapper/InquiryBoardMapper.java
@@ -1,8 +1,9 @@
 package com.user.backend.mapper;
 
-import com.user.backend.dto.InquiryDto;
+import com.user.backend.dto.InquiryBoardDto;
 import com.user.backend.dto.SearchConditionDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,7 +20,7 @@ public interface InquiryBoardMapper {
      * @param searchConditionDto 검색조건
      * @return 검색조건과 페이지네이션에 맞는 문의 게시물 리스트
      */
-    List<InquiryDto> selectBoardListByCondition(SearchConditionDto searchConditionDto);
+    List<InquiryBoardDto> selectBoardListByCondition(@Param("searchCondition") SearchConditionDto searchConditionDto, @Param("memberId") String memberId);
 
     /**
      * SELECT totalRowCount By searchCondition
@@ -27,7 +28,7 @@ public interface InquiryBoardMapper {
      * @param searchConditionDto 검색조건
      * @return 검색조건 맞는 문의 게시물 리스트 의 수
      */
-    int selectTotalRowCountByCondition(SearchConditionDto searchConditionDto);
+    int selectTotalRowCountByCondition(@Param("searchCondition") SearchConditionDto searchConditionDto, @Param("memberId") String memberId);
 
     /**
      * SELECT tb_inquiry_board By boardId
@@ -35,7 +36,7 @@ public interface InquiryBoardMapper {
      * @param boardId ( pk )
      * @return boardId와 일치하는 tb_inquiry_board
      */
-    Optional<InquiryDto> selectBoardById(Long boardId);
+    Optional<InquiryBoardDto> selectBoardById(Long boardId);
 
     /**
      * DELETE tb_inquiry_board By Id
@@ -51,4 +52,34 @@ public interface InquiryBoardMapper {
      * @param boardId ( pk )
      */
     void updateViewById(Long boardId);
+
+    /**
+     * SELECT tb_inquiry_board LIMIT 6
+     *
+     * @return 최신 게시물 6개
+     */
+    List<InquiryBoardDto> selectBoardListForMain();
+
+    /**
+     * INSERT tb_inquiry_board
+     *
+     * @param inquiryBoardDto ( author_id, title, content, is_secret )
+     */
+    void insertBoard(InquiryBoardDto inquiryBoardDto);
+
+    /**
+     * UPDATE tb_inquiry_board
+     *
+     * @param inquiryBoardDto ( title, content, is_secret )
+     */
+    void updateBoard(InquiryBoardDto inquiryBoardDto);
+
+    /**
+     * SELECT tb_inquiry_board By boardId And memberId
+     *
+     * @param boardId ( pk )
+     * @param memberId authorId
+     * @return boardId와 authorId가 일치하는 게시물
+     */
+    Optional<InquiryBoardDto> selectBoardByIdAndMemberId(@Param("boardId") Long boardId, @Param("memberId") String memberId);
 }
