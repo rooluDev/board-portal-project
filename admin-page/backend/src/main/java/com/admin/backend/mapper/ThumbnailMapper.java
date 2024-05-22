42a63072d3559c600295e5a99bded0555e835b1f
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-22 19:19:56 +0900

    docs : 주석추가

--- admin-page/backend/src/main/java/com/admin/backend/mapper/ThumbnailMapper.java
+++ admin-page/backend/src/main/java/com/admin/backend/mapper/ThumbnailMapper.java
@@ -3,10 +3,24 @@ package com.admin.backend.mapper;
 import com.admin.backend.dto.ThumbnailDto;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * Thumbnail Mapper
+ */
 @Mapper
 public interface ThumbnailMapper {
 
+    /**
+     * INSERT tb_thumbnail
+     *
+     * @param thumbnailDto (file_id, original_name, physical_name, file_path, extension, size)
+     */
     void insertThumbnail(ThumbnailDto thumbnailDto);
 
+    /**
+     * DELETE tb_thumbnail By id
+     *
+     * @param fileId ( pk )
+     * @return 삭제한 PK
+     */
     int deleteThumbnailByFileId(Long fileId);
 }
--- admin-page/backend/src/main/java/com/admin/backend/service/ThumbnailService.java
+++ admin-page/backend/src/main/java/com/admin/backend/service/ThumbnailService.java
@@ -2,9 +2,23 @@ package com.admin.backend.service;
 
 import com.admin.backend.dto.ThumbnailDto;
 
+/**
+ * Thumbnail Service
+ */
 public interface ThumbnailService {
 
+    /**
+     * Thumbnail DB 등록
+     *
+     * @param thumbnailDto  DB에 저장할 Thumbnail Dto
+     */
     void addThumbnail(ThumbnailDto thumbnailDto);
 
+    /**
+     * Thumbnail DB 삭제
+     *
+     * @param fileId ( pk )
+     * @return 삭제한 썸네일 PK
+     */
     int deleteThumbnailByFileId(Long fileId);
 }
