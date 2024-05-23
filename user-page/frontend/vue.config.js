c8428c0829c4844dffc508519d4ba3fd57d6216a
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-05-24 04:47:45 +0900

    feat : target url env 변수

--- user-page/frontend/vue.config.js
+++ user-page/frontend/vue.config.js
@@ -1,22 +1,22 @@
-const { defineConfig } = require('@vue/cli-service');
+const {defineConfig} = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-  outputDir: './build',
+    transpileDependencies: true,
+    lintOnSave: false,
+    outputDir: './build',
 
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081/',
-        changeOrigin: true,
-      },
+    devServer: {
+        proxy: {
+            '/api': {
+                target: process.env.VUE_APP_API_URL,
+                changeOrigin: true,
+            },
+        },
     },
-  },
 
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
+    pluginOptions: {
+        vuetify: {
+            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+        }
+    }
 });
