b6a292b232310dc5c7c7e8430b518a5713e25d10
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-08-01 01:33:16 +0900

    feat : Notice Board Entity

---
+++ user-page/backend/src/main/java/com/user/backend/entity/NoticeBoard.java
@@ -0,0 +1,54 @@
+package com.user.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+/**
+ * Notice Board Entity
+ */
+@Entity
+@Table(name = "tb_notice_board")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoticeBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long boardId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Admin author;
+
+    @Column(name = "title", nullable = false, length = 99)
+    private String title;
+
+    @Column(name = "content", nullable = false, length = 3999)
+    private String content;
+
+    @Column(name = "views", nullable = false)
+    private Long views = 0L;
+
+    @Column(name = "is_fixed", nullable = false)
+    private Boolean isFixed = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "edited_at", nullable = true)
+    private Timestamp editedAt;
+}
