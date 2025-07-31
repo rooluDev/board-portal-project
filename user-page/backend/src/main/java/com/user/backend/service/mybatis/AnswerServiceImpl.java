5528b808c78dcbf416365bf43d5049c0e13a6c0b
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-08-01 01:05:52 +0900

    feat : AnswerService Qualifier Mybatis

--- user-page/backend/src/main/java/com/user/backend/service/mybatis/AnswerServiceImpl.java
+++ user-page/backend/src/main/java/com/user/backend/service/mybatis/AnswerServiceImpl.java
@@ -1,9 +1,9 @@
-package com.user.backend.service;
+package com.user.backend.service.mybatis;
 
 import com.user.backend.dto.AnswerDto;
 import com.user.backend.mapper.AnswerMapper;
+import com.user.backend.service.AnswerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,8 +11,7 @@ import java.util.Optional;
 /**
  * Answer Service 구현체
  */
-@Service
-@Primary
+@Service("answerMybatis")
 @RequiredArgsConstructor
 public class AnswerServiceImpl implements AnswerService {
 
