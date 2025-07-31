87bea61d5f0b4e03659a576798bae4ae7cbbf9a7
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-08-01 01:31:45 +0900

    feat : ModelMapperConfig

---
+++ user-page/backend/src/main/java/com/user/backend/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.user.backend.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ModelMapperConfig
+ */
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
